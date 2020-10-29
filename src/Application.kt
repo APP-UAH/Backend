8c0e8d6f34f64abfad243cd48c5f91263a2b1ae1
charlypalou <carlos@DESKTOP-3FCCCQB.localdomain>
2020-10-29 14:43:45 +0100

    initial commit

---
+++ src/Application.kt
@@ -0,0 +1,71 @@
+package com.appuah
+
+import com.appuah.routes.users
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.locations.*
+import io.ktor.sessions.*
+import io.ktor.auth.*
+import io.ktor.gson.*
+import io.ktor.features.*
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.*
+import io.ktor.http.content.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+
+    val server: NettyApplicationEngine = embeddedServer(Netty, port = 8080) {
+        install(Locations) {
+        }
+
+
+
+        install(CORS) {
+            method(HttpMethod.Options)
+            method(HttpMethod.Get)
+            method(HttpMethod.Post)
+            method(HttpMethod.Put)
+            method(HttpMethod.Delete)
+            method(HttpMethod.Patch)
+            header(HttpHeaders.Authorization)
+            allowCredentials = true
+            anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+        }
+
+        install(Locations) {
+        }
+
+        install(Sessions) {
+            cookie<MySession>("MY_SESSION") {
+                cookie.extensions["SameSite"] = "lax"
+            }
+        }
+
+        install(Authentication) {
+        }
+
+        install(ContentNegotiation) {
+            gson {
+            }
+        }
+
+        routing {
+
+            users()
+
+        }
+    }
+    server.start(wait = true)
+}
+
+const val API = "AppUah"
+
+data class MySession(val count: Int = 0)
+
