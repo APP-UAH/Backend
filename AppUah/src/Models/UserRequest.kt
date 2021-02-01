0fa22ce65c759066e13b62d0b68ca8a66dad91f4
Algoldu14 <alvaro.golbano@edu.uah.es>
2021-02-01 18:32:30 +0100

    AddUser routes created

---
+++ AppUah/src/Models/UserRequest.kt
@@ -0,0 +1,12 @@
+package Models
+
+data class UserRequest(val type:Int,
+                       val username:String,
+                       val password:String,
+                       val name:String,
+                       val surname:String,
+                       val phone_number:String,
+                       val office:String,
+                       val email:String,
+                       val is_associated:Boolean,
+                       val is_deputy:Boolean){}
